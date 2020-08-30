25514a9661de5c0bcce691ca16481dfe9cca4ab5
umindo <info@unitekmasindonesia.com>
2020-08-30 17:07:37 +0700

    v1.0.0

---
+++ it_management/config/it_management.py
@@ -0,0 +1,66 @@
+from __future__ import unicode_literals
+from frappe import _
+
+
+def get_data():
+	return [
+        {	
+        "label": _("Transaksi"),
+		"items":[
+				{
+					"type": "doctype",
+					"label": "Master Brand",
+					"name": "Master Brand",
+					"description": _("Imput masukan barang Beacukai."),
+				},
+				{
+					"type": "doctype",
+					"label": "Master User",
+					"name": "Master User",
+					"description": _("Opening for a Job."),
+				},
+				{
+					"type": "doctype",
+                                        "label": "Master Vendor IT",
+                                        "name": "Master Vendor IT",
+                                        "description": _("Opening for a Job."),
+                                },
+
+				{
+					"type": "doctype",
+					"label": "Hardware Asset",
+					"name": "Hardware Asset",
+					"description": _("Monthly salary statement."),
+				},
+				{
+					"type": "doctype",
+					"label": "Software Instance",
+					"name": "Software Instance",
+					"description": _("Monthly salary statement."),
+				},
+ 				 {
+                                        "type": "doctype",
+                                        "label": "IT Maintenance Cheklist",
+                                        "name": "IT Maintenance Cheklist",
+                                        "description": _("Monthly salary statement."),
+                                },
+
+
+				]
+		},
+		{
+        "label": _("Laporan"),
+        "items": 
+            [
+				{
+					"type": "report",
+					"name": "Laporan Pengeluaran Barang Per Dokumen Pabean",
+					"doctype": "Pengeluaran Barang",
+					"description": _("Employee records."),
+					"is_query_report": True,
+				},
+
+				
+			]
+        },
+	]
---
+++ it_management/it_management/doctype/master_user/master_user.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, UMINDO and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Master User', {
+	// refresh: function(frm) {
+
+	// }
+});
